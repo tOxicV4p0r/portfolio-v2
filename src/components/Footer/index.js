04621d2f3a60db748d26ccd3d9c28c298a5bd102
kraisorn <22193826+tOxicV4p0r@users.noreply.github.com>
2023-06-26 01:11:05 +0700

    fixed error

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -10,7 +10,7 @@ const data = {
 function Footer() {
 
     return (
-        <section className="space-y-5">
+        <section className="space-y-5 mb-10">
             <div className="flex justify-end">
                 <span className="text-primarySubContent1">{data.descriptions} {data.title} </span>
                 <a href={data.link} className="px-3" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></a>
