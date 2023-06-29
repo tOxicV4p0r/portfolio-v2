af60bdec0a990a29b43edfbe51abe5e089b34231
kraisorn <22193826+tOxicV4p0r@users.noreply.github.com>
2023-06-29 23:28:55 +0700

    change to grid layout

--- src/App.js
+++ src/App.js
@@ -71,13 +71,39 @@ function App() {
         }
       }
     } catch (e) { /* empty */ }
+
+    // navcontent
+    try {
+      const firstOffetTop = document.getElementById(SCROLL_SECTION_ID).getBoundingClientRect().top;
+
+      for (let i = 0; i < data.sections.length; i++) {
+        const el = data.sections[i];
+        const elementOffetTop = document.getElementById(el.sectionId).getBoundingClientRect().top;
+        const height = document.getElementById(el.sectionId).getClientRects()[0].height;
+        const rootHeight = document.getElementById('root').getClientRects()[0].height * 0.4;
+
+        if (elementOffetTop <= firstOffetTop) {
+          if (elementOffetTop + (height / 2) > firstOffetTop) {
+            setCurrectSection(el.sectionId);
+          }
+        } else if (elementOffetTop > firstOffetTop && elementOffetTop < rootHeight) {
+          setCurrectSection(el.sectionId);
+        }
+      }
+    } catch (e) { /* empty */ }
   };
 
   const handleWheel = (e) => {
-    const offSet = document.getElementById(SCROLL_SECTION_ID).getBoundingClientRect().top;
-    const firstOffetTop = document.getElementById(data.sections[0].sectionId).getBoundingClientRect().top - offSet;
-    const startPosition = Math.abs(firstOffetTop);
-    document.getElementById(SCROLL_SECTION_ID).scroll(0, startPosition + e.deltaY);
+    /*
+    // no handle inside content section
+    const { x, y, width, height } = document.getElementById(SCROLL_SECTION_ID).getClientRects()[0];
+    if ((e.clientX < x || e.clientX > (x + width)) && (e.clientY > y || e.clientY < (y + height))) {
+      const offSet = document.getElementById(SCROLL_SECTION_ID).getBoundingClientRect().top;
+      const firstOffetTop = document.getElementById(data.sections[0].sectionId).getBoundingClientRect().top - offSet;
+      const startPosition = Math.abs(firstOffetTop);
+      document.getElementById(SCROLL_SECTION_ID).scroll(0, startPosition + (e.deltaY * 0.5));
+    }
+    */
   }
 
   useEffect(() => {
@@ -93,13 +119,15 @@ function App() {
 
   return (
     <main className="max-w-6xl mx-auto" >
-      <div className="px-5 pb-7 pt-14 font-poppins tracking-wide block lg:flex h-screen">
-        <div className="w-full lg:w-5/6 grid gap-10 pb-10 lg:pb-0">
-          <Header />
-          {isNonMobile ? <NavContent data={data} section={currectSection} scrollSectionId={SCROLL_SECTION_ID} fisrtSectionOfScroll={data.sections[0].sectionId} /> : null}
-          <Contact />
+      <div className="grid lg:grid-cols-[2fr_3fr] px-5 pb-7 pt-14 font-poppins tracking-wide">
+        <div>
+          <div className="sticky top-14 h-[90vh] flex flex-col justify-between">
+            <Header />
+            {isNonMobile ? <NavContent data={data} section={currectSection} scrollSectionId={SCROLL_SECTION_ID} fisrtSectionOfScroll={data.sections[0].sectionId} /> : null}
+            <Contact />
+          </div>
         </div>
-        <div id={SCROLL_SECTION_ID} className="w-full overflow-y-scroll no-scrollbar grid gap-32 lg:gap-40" onScroll={handleScroll}>
+        <div id={SCROLL_SECTION_ID} className="w-full no-scrollbar grid gap-32 lg:gap-40" onScroll={handleScroll}>
           <About
             detail={data.sections[data.sections.findIndex(e => e.title === "About")]}
           />
